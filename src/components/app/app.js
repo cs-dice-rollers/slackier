fe83068fe7b0f09ba7f75644ef463973a08e653f
Dylan Smith <dylantsmith@gmail.com>
2017-06-29 12:05:59 -0500

    (Re)Create app component
    - We had to do this because of case sensitive issues with git

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './app.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
+      <p>This is the app</p>
+    </div>
+  );
+}
