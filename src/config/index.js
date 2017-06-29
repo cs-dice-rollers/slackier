234a16d78b1ec752c69c75ab6f2010e9c1958f61
Dylan Smith <dylantsmith@gmail.com>
2017-06-29 14:37:05 -0500

    Socket functions
    - Refactor out send and receive functions to a library
    - Use environment and/or config values for strings

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
 const config = {
+  eventNames: {
+    sendMessage: 'CLIENT_MESSAGE',
+    receiveMessage: 'SERVER_MESSAGE',
+  },
   socketServer: process.env.SOCKET_SERVER || 'http://localhost:3000',
 };
 
