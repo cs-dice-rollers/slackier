e9f3c6c697e74ad9da206e707f5d48afec09ae26
Michael Young <michaeljy@gmail.com>
2017-06-29 11:36:16 -0500

    Webpack working.

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
